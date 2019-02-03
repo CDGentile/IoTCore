76560d4093fa6a1120c6fc405da0e9609548f82b
Chris Gentile <cdgentile@gmail.com>
2019-02-02 18:30:15 -0800

    NTP time working

--- IoTCore.ino
+++ IoTCore.ino
@@ -13,6 +13,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <FS.h>
+#include <TimeLib.h>
+#include <WiFiUdp.h>
 
 
 //comment out to stop serial comms
@@ -30,12 +32,80 @@
 
 const bool ledEnable = false;
 
+static const char ntpServerName[] = "us.pool.ntp.org";
+const int timeZone = -8;
+
 ESP8266WebServer server(80);
 File fsUploadFile;              // a File object to temporarily store the received file
 
+WiFiUDP Udp;
+unsigned int localPort = 8888;
+time_t getNtpTime();
+
+void setupWifi();
+void setupOTA();
+void setupUdp();
+
+String getContentType(String filename);
+bool handleFileRead(String path);
+void handleFileUpload();
+void handleRoot();
+void uploadPage(void);
+void setupServer(void);
+
+void digitalClockDisplay();
+void printDigits(int digits);
+void sendNTPpacket(IPAddress &address);
+
+
+
+void setup() {
+
+  //begin serial debug terminal if required
+  #ifdef DEBUG
+    Serial.begin(115200);
+  #endif
+
+  delay(250);
+
+  //Initialize WiFi - todo: what to do if connection fails?
+  setupWifi();
+
+  //Function calls required for OTA updates
+  //DEBUG mode required for troubleshooting
+  setupOTA();
+
+  SPIFFS.begin();                           // Start the SPI Flash Files System
+
+  setupServer();
+
+  setupUdp();
+}
+
+time_t prevDisplay = 0; // when the digital clock was displayed
+
+void loop()
+{
+
+  //mandatory handlers
+
+  server.handleClient();
+  ArduinoOTA.handle();
+
+  if (timeStatus() != timeNotSet) {
+  if (now() != prevDisplay) { //update the display only if time has changed
+    prevDisplay = now();
+    digitalClockDisplay();
+  }
+}
+
+  delay(500);
+
+}
+
 
 //WiFi Setup routine - modify to include LED status feedback
-void setup_wifi() {
+void setupWifi() {
   WiFiManager wifiManager;
 
 IPAddress _ip = IPAddress(192, 168, 1, 205);
@@ -49,7 +119,7 @@ wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
   wifiManager.autoConnect("IoTCore");
 }
 
-void setup_OTA() {
+void setupOTA() {
   ArduinoOTA.onStart([]() {
     DEBUG_PRINTLN("Start");
   });
@@ -70,6 +140,101 @@ void setup_OTA() {
   ArduinoOTA.begin();
 }
 
+//time functions
+void setupUdp() {
+  DEBUG_PRINTLN("Starting UDP");
+  Udp.begin(localPort);
+  DEBUG_PRINT("Local port: ");
+  DEBUG_PRINTLN(Udp.localPort());
+  DEBUG_PRINTLN("waiting for sync");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
+}
+
+void digitalClockDisplay()
+{
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(".");
+  Serial.print(month());
+  Serial.print(".");
+  Serial.print(year());
+  Serial.println();
+}
+
+void printDigits(int digits)
+{
+  // utility for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  DEBUG_PRINTLN("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  DEBUG_PRINT(ntpServerName);
+  DEBUG_PRINT(": ");
+  DEBUG_PRINTLN(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      DEBUG_PRINTLN("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  DEBUG_PRINTLN("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+/// WebServer functions
 String getContentType(String filename) { // convert the file extension to the MIME type
   if (filename.endsWith(".html")) return "text/html";
   else if (filename.endsWith(".css")) return "text/css";
@@ -81,7 +246,7 @@ String getContentType(String filename) { // convert the file extension to the MI
 }
 
 bool handleFileRead(String path) { // send the right file to the client (if it exists)
-  Serial.println("handleFileRead: " + path);
+  DEBUG_PRINTLN("handleFileRead: " + path);
   if (path.endsWith("/")) path += "index.html";          // If a folder is requested, send the index file
   String contentType = getContentType(path);             // Get the MIME type
   String pathWithGz = path + ".gz";
@@ -91,10 +256,10 @@ bool handleFileRead(String path) { // send the right file to the client (if it e
     File file = SPIFFS.open(path, "r");                    // Open the file
     size_t sent = server.streamFile(file, contentType);    // Send it to the client
     file.close();                                          // Close the file again
-    Serial.println(String("\tSent file: ") + path);
+    DEBUG_PRINTLN(String("\tSent file: ") + path);
     return true;
   }
-  Serial.println(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
+  DEBUG_PRINTLN(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
   return false;
 }
 
@@ -103,7 +268,7 @@ void handleFileUpload(){ // upload a new file to the SPIFFS
   if(upload.status == UPLOAD_FILE_START){
     String filename = upload.filename;
     if(!filename.startsWith("/")) filename = "/"+filename;
-    Serial.print("handleFileUpload Name: "); Serial.println(filename);
+    DEBUG_PRINT("handleFileUpload Name: "); DEBUG_PRINTLN(filename);
     fsUploadFile = SPIFFS.open(filename, "w");            // Open the file for writing in SPIFFS (create if it doesn't exist)
     filename = String();
   } else if(upload.status == UPLOAD_FILE_WRITE){
@@ -112,7 +277,7 @@ void handleFileUpload(){ // upload a new file to the SPIFFS
   } else if(upload.status == UPLOAD_FILE_END){
     if(fsUploadFile) {                                    // If the file was successfully created
       fsUploadFile.close();                               // Close the file again
-      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      DEBUG_PRINT("handleFileUpload Size: "); DEBUG_PRINTLN(upload.totalSize);
       server.sendHeader("Location","/success.html");      // Redirect the client to the success page
       server.send(303);
     } else {
@@ -156,38 +321,4 @@ void setupServer(void) {
   server.begin();
   DEBUG_PRINTLN("HTTP Server Started.");
 }
-
-
-void setup() {
-
-  //begin serial debug terminal if required
-  #ifdef DEBUG
-    Serial.begin(115200);
-  #endif
-
-  delay(250);
-
-  //Initialize WiFi - todo: what to do if connection fails?
-  setup_wifi();
-
-  //Function calls required for OTA updates
-  //DEBUG mode required for troubleshooting
-  setup_OTA();
-
-  SPIFFS.begin();                           // Start the SPI Flash Files System
-
-  setupServer();
-}
-
-
-void loop()
-{
-
-  //mandatory handlers
-
-  server.handleClient();
-  ArduinoOTA.handle();
-
-  delay(500);
-
-}
+/// end WebServer functions
